d4599dc85a078dd37fb781d6f0b96438956bdb8c
Vinicius Nunes Martins <nunesmartinsvinicius@gmail.com>
2025-12-05 23:02:29 -0300

    feat(ui): add ng-icons and update styles

--- src/app/pages/whoami/components/header/header.component.ts
+++ src/app/pages/whoami/components/header/header.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  bootstrapGeoAlt,
+  bootstrapEnvelope,
+  bootstrapTelephone,
+  bootstrapLinkedin,
+  bootstrapGithub,
+} from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './header.component.html',
-  host: { class: 'block' }
+  host: { class: 'block' },
+  viewProviders: [
+    provideIcons({
+      bootstrapGeoAlt,
+      bootstrapEnvelope,
+      bootstrapTelephone,
+      bootstrapLinkedin,
+      bootstrapGithub,
+    }),
+  ],
 })
 export class HeaderComponent {
 
--- src/app/pages/whoami/components/projects/projects.component.ts
+++ src/app/pages/whoami/components/projects/projects.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  bootstrapLightningCharge,
+  bootstrapEmojiSmile,
+  bootstrapCpu,
+} from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './projects.component.html',
-  host: { class: 'block' }
+  host: { class: 'block' },
+  viewProviders: [
+    provideIcons({
+      bootstrapLightningCharge,
+      bootstrapEmojiSmile,
+      bootstrapCpu,
+    }),
+  ],
 })
 export class ProjectsComponent {
 
